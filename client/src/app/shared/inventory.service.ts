03ad30d773ea0001bd13a352ef692be24216d2f2
manikantav <76819350+manikantavoruganti@users.noreply.github.com>
2023-11-22 21:14:40 -0700

    Update inventory.service.ts

--- client/src/app/shared/inventory.service.ts
+++ client/src/app/shared/inventory.service.ts
@@ -11,7 +11,7 @@ export class InventoryService {
 
   constructor(private fb:FormBuilder, private http: HttpClient) { }
   
-  readonly baseURL = 'https://store-management-system-y5ye.onrender.com';
+  readonly baseURL = 'https://store-management-system-y5ye.onrender.com/api';
   list: Inventory[] = [];
 
     inventoryForm = this.fb.group({
