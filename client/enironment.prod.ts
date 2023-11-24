c82e705269f34ac92231aee0a0eac198bacceeb0
manikantav <76819350+manikantavoruganti@users.noreply.github.com>
2023-11-24 06:34:32 -0700

    Update enironment.prod.ts

--- client/enironment.prod.ts
+++ client/enironment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: 'https://store-management-system-y5ye.onrender.com/api',
+  baseURL: 'https://store-management-system-y5ye.onrender.com/api:splat',
 };
