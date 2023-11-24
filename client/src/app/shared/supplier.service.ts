b50f0b33f949c2df76e4b4295f282cce3fc05ddd
manikantav <76819350+manikantavoruganti@users.noreply.github.com>
2023-11-24 08:46:14 -0700

    Update supplier.service.ts

--- client/src/app/shared/supplier.service.ts
+++ client/src/app/shared/supplier.service.ts
@@ -10,7 +10,7 @@ export class SupplierService {
 
   constructor(private fb:FormBuilder, private http: HttpClient) { }
   
-  readonly baseURL = 'https://store-management-system-y5ye.onrender.com/api/suppliers/';
+  readonly baseURL = 'https://store-management-system-y5ye.onrender.com/api/suppliers';
   list: Supplier[] = [];
 
     supplierForm = this.fb.group({
